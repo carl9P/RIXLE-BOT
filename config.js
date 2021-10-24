f94f2ea66789d79c014b84a2ed03ef1a0bc8357e
00300 <90952986+well300@users.noreply.github.com>
2021-10-24 11:30:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,25 +1,25 @@
 userbot = {
- owner: [ "62857420881846","628812904283","6282328303332", "62822980698995", "436504463151666", "62823283033323" ],
+ owner: [ "917842346461" ],
   MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
    mess: {
-   wait: "tunggu sedang di proses...",
+   wait: "wait is in process...",
    error: "errorr!!"
    },
-   prefix: ["/"],
+   prefix: [")"],
    gexp: 50,
    limit: 100,
-   eror: "Sepertinya Eror",
-   waits: "Sedang di proses",
+   eror: "It seems like Error",
+   waits: "Being processed",
    butmag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTHDLrd0_DwHnJfuZ_pRMOAbLi3-ot-NxyPA&usqp=CAU', // buat Image Button
-   packname: "RIXLE-BOT",
-   author: '@_RizkyAdi',
+   packname: "Well...",
+   author: '@300',
     setting: {
     admin: "only admin",
     group: "only group",
     owner: "owner only pack",
     jadibot: "jadibot only",
-    botadmin: "bot harus menjadi admin",
-    nsfw: "FITUR BELUM DIAKTIFKAN!"
+    botadmin: "bot must be admin",
+    nsfw: "FEATURES HAVE NOT BEEN ACTIVATED!"
     }
 }
 module.exports = userbot
